3c28382c4fb3521d069a57c65f2d43d29ea76043
Aleksandr <42320109+zhukovskiial@users.noreply.github.com>
2025-03-23 21:39:22 +0300

    Update comments for KNN.py

--- KNN.py
+++ KNN.py
@@ -1,31 +1,30 @@
 import numpy as np
 
 class KNN:
-    # инициализация класса
     def __init__(self, k=3):
         self.k = k
     
-    # расчет расстояния евклидовой метрикой
+    # distance estimation using euclidian metric
     def euclidean_distance(self, x1, x2):
         return np.sqrt(np.sum((x1 - x2) ** 2))
 
-    # передача данных для обучения
+    # getting train data
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
 
-    # метод для получения предсказаний
+    # predict method
     def predict(self, X_test):
         predictions = []
-        # для каждого элемента X_test
+        # for each X_test element
         for x in X_test:
-            # рассчитывается массив с расстояниями элемента до каждого элемента X_train
+            # calculate the array with distances of the element to each element of X_test
             distances = [self.euclidean_distance(x, x_train) for x_train in self.X_train]
-            # записываются индексы k элементов X_train, расстояние до которых наименьшее
+            # record indexes of k elements X_train with the smallest distance
             k_indices = np.argsort(distances)[:self.k]
-            # по индексам определяются метки k элементов из y_train
+            # the indexes are used to determine the labels of k elements from y_train.
             k_nearest_labels = [self.y_train[i] for i in k_indices]
-            # из k меток выбирается самая частая и записывается в предсказание для элемента из X_test
+            # the most frequent one of the k labels is selected and recorded in the prediction for the element from X_test
             most_common = np.bincount(k_nearest_labels).argmax()
             predictions.append(most_common)
         return predictions
