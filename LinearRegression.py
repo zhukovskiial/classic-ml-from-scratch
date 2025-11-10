767693155ce33e27b213f3c9b88c3e3fb3d7ab5d
Aleksandr <42320109+zhukovskiial@users.noreply.github.com>
2025-11-10 10:19:16 +0300

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,4 +1,4 @@
-# linear regression based on gradient descent approach
+# реализация простой линейной регрессии с обучением методом градиентного спуска
 
 import numpy as np
 
@@ -10,19 +10,23 @@ class LinearRegressionGradient:
 
         def fit(self, X, y):
             n_samples, n_features = X.shape
+            # инициализируем веса и смещение нулями (можно случайными значениями)
             self.bias, self.weights = 0, np.zeros(n_features)
             prev_db, prev_dw = 0, np.zeros(n_features)
 
         for _ in range(self.n_iters):
             y_pred = np.dot(X, self.weights) + self.bias
+            # dw и db - производные по переменным весов (w) и смещения (b)
             db = 1 / n_samples * np.sum(y_pred - y)
             dw = 1 / n_samples * np.dot(X.T, (y_pred - y))
+            # шагаем в обратную сторону от направления производной с заданным шагом learning_rate
             self.bias -= self.learning_rate * db
             self.weghts -= self.learning_rate * dw
 
             abs_db_reduction = np.abs(db - prev_db)
             abs_dw_reduction = np.abs(dw - prev_dw)
 
+            # если шаг обучения меньше заданного порога, то прекращаем обучение
             if abs_db_reduction < self.threshold:
                 if abs_dw_reduction.all() < self.threshold:
                     break
